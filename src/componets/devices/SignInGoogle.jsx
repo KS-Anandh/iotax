9d4f9fe6e9e80861d2f8311c70188fdd6815ef00
KS-Anandh <167880483+KS-Anandh@users.noreply.github.com>
2024-10-20 21:10:59 +0530

    Update SignInGoogle.jsx

--- src/componets/devices/SignInGoogle.jsx
+++ src/componets/devices/SignInGoogle.jsx
@@ -4,18 +4,21 @@ import {signInWithPopup} from 'firebase/auth'
 import axios from 'axios';
 import google from '../../../assets/google.png'
 import { useNavigate } from 'react-router-dom';
-const SignInGoogle = () => {
+const SignInGoogle = ({setLoading}) => {
   const nav=useNavigate();
     const GoogleHandler=()=>{
+      setLoading(true);
     signInWithPopup(auth,provider).then((data)=>{
       const gmail=data.user.email;
       const name=data.user.displayName;
       axios.post("https://iotex-ajgn.vercel.app/users/gmail",{userName:name,userMail:gmail},{headers: {'Content-Type': 'application/json'}})
       .then((res)=>{
+        setLoading(false);
         localStorage.setItem("token",res.data);
         nav("/projects")
       })
       .catch((err)=>{
+        setLoading(false);
         alert(err)
       })
     })
